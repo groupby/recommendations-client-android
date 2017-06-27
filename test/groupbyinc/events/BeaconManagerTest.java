aff1a28ed09941c420de509de6f164cc11d69a73
Tahir Wasim <tahir.wasim@groupbyinc.com>
2017-06-26 23:17:27 -0400

    make wiremock work

--- test/groupbyinc/events/BeaconManagerTest.java
+++ test/groupbyinc/events/BeaconManagerTest.java
@@ -1,32 +1,35 @@
 package groupbyinc.events;
 
+import com.github.tomakehurst.wiremock.junit.WireMockRule;
 import com.groupbyinc.common.apache.http.HttpResponse;
 import com.groupbyinc.common.apache.http.HttpStatus;
-import com.meterware.pseudoserver.PseudoServer;
-import com.meterware.pseudoserver.PseudoServlet;
-import com.meterware.pseudoserver.WebResource;
 import groupbyinc.events.components.Customer;
 import groupbyinc.models.BeaconRequest;
 import groupbyinc.models.TrackerException;
+import groupbyinc.util.FreePort;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 
-import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.post;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
 import static org.junit.Assert.assertEquals;
 
 public class BeaconManagerTest {
 
-//  @Rule public WireMockRule mock = new WireMockRule(FreePort.next());
-  BeaconRequest beaconRequest;
+  @Rule public WireMockRule mock = new WireMockRule(FreePort.next());
+  private BeaconRequest beaconRequest;
 
   @Before
   public void setUpRequestAndClient() throws MalformedURLException {
     try {
       AddToCart event = (AddToCart)(new AddToCart().setId("testId").setCustomer(new Customer("custid", "custarea")));
       beaconRequest = new BeaconRequest(event);
+      beaconRequest.setRequestUrl(new URL("http", "localhost", mock.port(), "/beacon"));
     } catch (TrackerException e) {
       e.printStackTrace();
     }
@@ -34,36 +37,21 @@ public class BeaconManagerTest {
 
   @Test
   public void testSendAddToCart() throws Throwable {
-    PseudoServer server = new PseudoServer(5000);
-    server.setResource("/beacon", new PseudoServlet() {
-      @Override
-      public WebResource getResponse(String methodType) throws IOException {
-        //        postBodys.add(new String(getBody()));
-        return new WebResource("{test: 'test'}");
-      }
-    });
+    String testResponseString = "Test";
 
-    URL requestUrl = new URL("http://localhost:" + Integer.toString(server.getConnectedPort()) + "/beacon");
-    System.out.println(requestUrl);
-    beaconRequest.setRequestUrl(requestUrl);
-//    stubWithBodyAndCode(beaconRequest, testResponseString, HttpStatus.SC_OK);
+    stubWithBodyAndCode(testResponseString, HttpStatus.SC_OK);
 
     BeaconManager manager = new BeaconManager("testapikey");
     HttpResponse response = manager.send(beaconRequest);
 
     // ASSERTS
     assertEquals(HttpStatus.SC_OK, response.getStatusLine().getStatusCode());
-
-
-
-    //assertEquals(expectedObj, Mappers.readValue(response.getEntity().getContent(), Object.class, false));
-//    mock.verify(postRequestedFor(urlEqualTo("/beacon")).withRequestBody(equalToJson("{\"customerIds\":[\"customer1\"]}")));
   }
 
-//  private void stubWithBodyAndCode(BeaconRequest beaconRequest, String testResponseString, int statusCode) {
-//    mock.stubFor(post(urlEqualTo("/beacon")).willReturn(aResponse().withHeader("Content-Type", "application/json")
-//                                                                       .withStatus(statusCode)
-//                                                                       .withBody(testResponseString)));
-//  }
+  private void stubWithBodyAndCode(String testResponseString, int statusCode) {
+    mock.stubFor(post(urlEqualTo("/beacon")).willReturn(aResponse().withHeader("Content-Type", "application/json")
+                                                                       .withStatus(statusCode)
+                                                                       .withBody(testResponseString)));
+  }
 
 }
